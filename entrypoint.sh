a2a6c383e2f09bbe072f5dd56e69bbc1526993a9
Riddhesh Sanghvi <riddhesh237@gmail.com>
2023-03-02 22:35:08 +0530

    Update comment in entrypoint
    Signed-off-by: Riddhesh Sanghvi <riddhesh237@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,13 +15,13 @@ echo -e "\e[0;32m\n$message\n\e[0m"
 
 # If token are not set, exit with error.
 if [[ -z "$GH_BOT_TOKEN" ]] && [[ -z "$VAULT_TOKEN" ]]; then
-  echo $( echo -en "\033[31mERROR\033[0m: Secret GH_BOT_TOKEN or VAULT_TOKEN is missing. Please add it to this action for proper execution. \nRefer https://github.com/rtCamp/action-phpcs-code-review#github-token-creation for more information." )
-
+  printf "[\e[0;31mERROR\e[0m] Secret \`GH_BOT_TOKEN\` or \`VAULT_TOKEN\` is missing. Please add it to this action for proper execution.\nRefer https://github.com/rtCamp/action-phpcs-code-review#github-token-creation for more information.\n"
   exit 1
 fi
 
-main_script="/usr/local/bin/main.sh"
+# custom path for files to override default files
 custom_path="$GITHUB_WORKSPACE/.github/inspections/vip-go-ci/"
+main_script="/usr/local/bin/main.sh"
 
 if [[ -d "$custom_path" ]]; then
     rsync -a "$custom_path" /usr/local/bin/
